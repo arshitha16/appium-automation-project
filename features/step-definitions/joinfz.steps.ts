fc8d63336f21967fa376122ddb51fa2185156f67
Arshitha <69782281+arshitha16@users.noreply.github.com>
2025-07-29 15:30:45 +0530

    Update step definitions

--- features/step-definitions/joinfz.steps.ts
+++ features/step-definitions/joinfz.steps.ts
@@ -1,12 +1,14 @@
 import { Given, When, Then } from "@wdio/cucumber-framework";
 import joinfzPage from "../pageobjects/joinfz.page";
-import { strict as assert } from "assert";
+import * as assert from "assert";
 
 Given(/^user is on the Home tab$/, async () => {
-  await joinfzPage.homeTab.waitForDisplayed();
+  await joinfzPage.tapIfExists(joinfzPage.btnPopup);
+  await joinfzPage.homeTab.waitForDisplayed({ timeout: 10000 });
 });
 
 When(/^joins the event from search$/, async () => {
+  await joinfzPage.btnSearch.waitForDisplayed({ timeout: 10000 });
   await joinfzPage.searchEvent();
 });
 
@@ -17,58 +19,4 @@ Then(/^user should be able to see the Fanzone$/, async () => {
     true,
     "Fanzone header is not displayed"
   );
-});
-
-Given(/^user is on the Fanzone$/, async () => {
-  console.log("Joined fanzone ", await joinfzPage.fzHeader.isDisplayed());
-});
-
-When(
-  /^user taps the chat field and types a message "([^"]*)"$/,
-  async (message: string) => {
-    console.log(`Sending chat message: ${message}`);
-    await joinfzPage.txtChat.waitForDisplayed({ timeout: 10000 });
-    await joinfzPage.txtChat.setValue(message);
-  }
-);
-
-Then(/^the send button should be visible$/, async () => {
-  await joinfzPage.btnSendMsg.waitForDisplayed({ timeout: 5000 });
-  assert.equal(
-    await joinfzPage.btnSendMsg.isDisplayed(),
-    true,
-    "Send button is not visible"
-  );
-});
-
-When(/^user taps the send button$/, async () => {
-  await joinfzPage.btnSendMsg.click();
-});
-
-Then(
-  /^user message "([^"]*)" should be sent in the chat$/,
-  async (expectedMessage: string) => {
-    console.log("Expected message ", expectedMessage);
-    assert.strictEqual(
-      await joinfzPage.messageValidate(),
-      expectedMessage,
-      "Sent message does not match the expected message"
-    );
-  }
-);
-
-When(/^user taps the GIF button$/, async () => {
-  await joinfzPage.btnGif.click();
-});
-
-Then(/^the GIF panel should open up$/, async () => {
-  await joinfzPage.txtSearchGif.waitForDisplayed({ timeout: 5000 });
-});
-
-When(/^user taps on a GIF$/, async () => {
-  await joinfzPage.searchGIF();
-});
-
-Then(/^the GIF should be sent in the chat$/, () => {
-  return true;
 });
